cfe308a4ff7223004f2b0b9bd6ff8980fc064da0
Kavindu Manaram <kavindu@calcey.com>
2018-05-17 13:34:47 +0530

    created the project

---
+++ server/src/Alpha/Alpha/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Alpha.Startup))]
+namespace Alpha
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
